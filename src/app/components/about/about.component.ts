1b5ecc334f898f07374001437064f21fbc06d83a
Raymond Gakwaya <raymond42.gr@gmail.com>
2023-05-10 04:59:21 +0200

    Initial commit

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+})
+export class AboutComponent {
+  constructor(private router: Router) {}
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+}
